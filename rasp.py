4ba3d951ca2740d7983b47e2f1cfcd24a019e206
Erico Oliveira <ericolvr@gmail.com>
2024-08-27 15:43:45 -0300

    rasp mocked server

--- rasp.py
+++ rasp.py
@@ -3,20 +3,28 @@ import random
 import string
 import time
 
+
+uniorgs = ["001-0001", "001-0002", "001-0003", "001-0004", "001-0005"]
+branches = ["Aldeota", "Messejana", "Parangaba", "Embú Guacú", "Parapiacabac"]
+alerts = ["Falha de Conexao", "Sensor de Movimento", "Gerador Disparndo", "Porta Aberta", "Alarme de Incendio"]
+
 def send_messages(endpoint, num_messages=100, delay=1):
     for _ in range(num_messages):
-        # Generate a random message
-        message = ''.join(random.choices(string.ascii_letters + string.digits, k=20))
-        data = {'message': message}
+    
+        uniorg = random.choice(uniorgs)
+        branch = random.choice(branches)
+        alert = random.choice(alerts)
+        data = {"uniorg": uniorg, "branch": branch, "alert": alert}
 
         # Send the POST request
         response = requests.post(endpoint, json=data)
+    
 
         # Check the response
         if response.status_code != 200:
-            print(f"Failed to send message: {message}")
+            print(f"Failed to send message: {data}")
         else:
-            print(f"Successfully sent message: {message}")
+            print(f"Successfully sent message: {data}")
 
         # Delay before the next message
         time.sleep(delay)
